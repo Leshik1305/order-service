d83fe87d89326f5e316fd8eae23cc32bbcde0f8f
Leshik1305 <realleshik1305@gmail.com>
2026-01-25 14:01:31 +0300

    fix settings.py

--- src/settings.py
+++ src/settings.py
@@ -6,10 +6,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_PORT: int = 8000
-    CALLBACK_URL: str = "http://localhost:8000/callback"
+    CALLBACK_URL: str = "https://leshik1305-order-service.dev-1.python-labs.ru/api"
     POSTGRES_CONNECTION_STRING: str
-    BASE_URL: Optional[str] = "http://localhost"
-    API_KEY: Optional[str] = "default_key"
+    BASE_URL: Optional[str] = "https://capashi.dev-1.python-labs.ru"
+    API_KEY: Optional[str] = "px64kgKjUAwnRlHuGuE5mk0zF7gOkYHa6L12qTdjzTg"
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "..", ".env"),
         env_file_encoding="utf-8",

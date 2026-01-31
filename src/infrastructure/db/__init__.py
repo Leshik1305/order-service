0c84f4614fed2597cc021262314d046b49ff66bc
Leshik1305 <realleshik1305@gmail.com>
2026-01-31 17:08:29 +0300

    drop db

--- src/infrastructure/db/__init__.py
+++ src/infrastructure/db/__init__.py
@@ -28,6 +28,10 @@ class Database:
         async with self.engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
 
+    async def drop_database(self) -> None:
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
     @contextlib.asynccontextmanager
     async def connection(self) -> typing.AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
